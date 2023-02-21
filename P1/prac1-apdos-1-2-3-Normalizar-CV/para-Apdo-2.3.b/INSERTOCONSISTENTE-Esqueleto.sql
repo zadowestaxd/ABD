a2b19ba53c0d30303bea18c7bd25129f6cda4bf2
Alejandro Valencia Blancas <alezombi@gmail.com>
2023-02-21 12:19:15 +0100

    Haciendo p1

--- P1/prac1-apdos-1-2-3-Normalizar-CV/para-Apdo-2.3.b/INSERTOCONSISTENTE-Esqueleto.sql
+++ P1/prac1-apdos-1-2-3-Normalizar-CV/para-Apdo-2.3.b/INSERTOCONSISTENTE-Esqueleto.sql
@@ -4,7 +4,7 @@
 create or replace PROCEDURE insertoConsistente (
 
 DNI_p        invierte.DNI%TYPE,
-NombreE_p    invierte.NombreE%TYPE,
+NOMBREE_p    invierte.NOMBREE%TYPE,
 Cantidad_p   invierte.Cantidad%TYPE,
 Tipo_p       invierte.Tipo%TYPE
 )  AS
@@ -14,7 +14,7 @@ Tipo_p       invierte.Tipo%TYPE
 es_dni_nuevo INT;
 es_tipo_nuevo INT;
 es_nombre_nuevo INT;
-
+temp char;
 -- VARS para excepciones 
       Tcoderror NUMBER;
       Ttexterror VARCHAR2(100);
@@ -26,16 +26,16 @@ BEGIN
 
 -- Decido si ese DNI es nuevo (no tiene inversiones) : si 0 es nuevo
 select count(*) into es_dni_nuevo from Invierte
-where DNI == DNI_p;
+where DNI = DNI_p;
 
 -- Decido si es Tipo nuevo para ese cliente: si 0 es nuevo
 
 select count(*) into es_tipo_nuevo from Invierte
-where TIPO == TIPO_p;
+where TIPO = TIPO_p and DNI_p = DNI;
  
 -- Decido si es empresa nueva para ese cliente: si 0 es nuevo
 select count(*) into es_nombre_nuevo from Invierte
-where Nombre == NombreE_p;
+where NOMBRE = NOMBREE_p;
 
 DBMS_output.put_line('Aqu� empieza INSERTOCONSISTENTE');
 
@@ -47,10 +47,10 @@ DBMS_output.put_line('muestro los datos de entrada ' || es_dni_nuevo || ' ' || e
 
 -- CASO 0.- No hay inversiones de ese DNI: inserto la fila y termino
 IF es_dni_nuevo = 0 THEN
-INSERT to Invierte (DNI_p, TIPO_p, NULL, NombreE_p);
+INSERT to Invierte values (DNI_p, TIPO_p, Cantidad_p, NOMBREE_p);
 --- imprimo la fila 
 
-DBMS_output.put_line(DNI_p || ' ' || TIPO_p || ' ' || es_dni_nuevo || ' ' || NombreE_p);
+DBMS_output.put_line(DNI_p || ' ' || TIPO_p || ' ' || es_dni_nuevo || ' ' || NOMBREE_p);
 
 
   --- ==  RESTO DE CASOS (tenemos el mismo DNI que en alguna fila de Invierte == ------
@@ -60,26 +60,40 @@ ELSE
 
 
 -- imprimo en el caso que estamos
-DBMS_output.put_line(
+
 
 -- CASO 1.-  Ya existe una fila con mismo Tipo (1) y Empresa (1) : es un error, no se lo permito 
-
-
+if es_tipo_nuevo > 0 and es_nombre_nuevo > 0 THEN
+DBMS_output.put_line('es un error, no se lo permito');
 -- CASO 2.-  tipo nuevo para una  Empresa que ya hay inversiones: debo insertar filas con ese tipo para todas sus empresas
+ELSIF es_nombre_nuevo > 0 and es_tipo_nuevo = 0 THEN
+   << outer_loop >> 
+   FOR temp IN (select Distinct NOMBREE FROM Inverte where DNI = DNI_p) LOOP 
+   Insert into Inverte values (DNI_p, TIPO_p, Cantidad_p, temp.NOMBREE);
+   DBMS_output.put_line(DNI_p || ' ' || TIPO_p || ' ' || Cantidad_p || ' ' || temp);
+   END loop outer_loop;
 
 
     -- Imprimo cada fila nueva
 
 
--- CASO 3.- Empresa nueva para un tipo que ya hay inversiones: debo insertar filas con ese empresa para todos sus tipos
+-- CASO 3.- Empresa nueva para un tipo que ya hay inversiones: debo insertar filas con ese empresa para todos sus TIPO
 --          No tomo en cuenta la nueva cantidad (es complejo comprobar la antig�a si hay varias empresas con ese Tipo)
 
-
+ELSIF es_nombre_nuevo = 0 and es_tipo_nuevo > 0
+ << outer_loop >> 
+   FOR temp IN (select Distinct TIPO FROM Inverte where DNI = DNI_p) LOOP 
+   Insert into Invierte values (DNI_p, temp.TIPO, Cantidad_p, NOMBREE_p);
+   DBMS_output.put_line(DNI_p || ' ' || temp.TIPO || ' ' || Cantidad_p || ' ' || NOMBREEmpresa);
+   END loop outer_loop;
 -- Imprimo cada fila nueva
 
-
 -- CASO 4.- El tipo y la empresa son nuevos: Como  CASO 2 + CASO 3
-
+  ELSIF es_nombre_nuevo = 0 and es_tipo_nuevo = 0 THEN
+  FOR temp IN (SELECT Distinct NOMBREE FROM invierte)
+ Insert into Invierte values (DNI_p, temp.Tipo_p, Cantidad_p, temp.NOMBREE);
+ DBMS_output.put_line(DNI_p || ' ' || temp.TIPO || ' ' || Cantidad_p || ' ' || temp.NOMBREE);
+ END loop;
   ------ para cada empresa que hab�a tengo que insertar el bono nuevo
 
    -- Imprimo cada fila nueva
