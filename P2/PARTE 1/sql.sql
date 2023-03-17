b79615eb891f50a236a1deafb960c3ef04005baf
Alejandro Valencia Blancas <alezombi@gmail.com>
2023-03-17 11:11:02 +0100

    parcial hecho

--- P2/PARTE 1/sql.sql
+++ P2/PARTE 1/sql.sql
@@ -4,15 +4,12 @@ plsql_block VARCHAR(2000);
 BEGIN
 select count(*)  into temp from user_objects where SEC = 'sec_'%nombreEmpresa; --TENGO QUE TRANSFORMAR NOMBREEMPRESA TODAS LAS LETRAS JUNTAS Y EN MAYÚSCULAS
 IF temp = 0
-START WITH 1
-INCREMENT BY 1 nomaxvalue
- CREATE SEQUENCE sec_%nombreEmpresa --VA DENTRO DEL PLSQLBLOCK
  plsql_block := 'create table ' || NombreTabla || 
- '(DNI CHAR(8) not null, 
- NombreC CHAR(30), 
- Direccion VARCHAR2(50),
- Telefono CHAR(12),
- PRIMARY KEY (DNI))' ;
+ 'start with 1
+increment by 1
+minvalue 0
+maxvalue 100
+cycle;' ;
 EXECUTE IMMEDIATE plsql_block;
 DBMS_output.put_line('--- tabla ' || NombreTabla || ' creada '); 
 commit;
